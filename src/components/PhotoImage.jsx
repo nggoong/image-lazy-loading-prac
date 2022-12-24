06acc51cb3d8304ec2133062d6f442c95abb9ff4
nggoong <nggoong@naver.com>
2022-12-24 14:49:18 +0900

    feat: image별로 화면에 나타나는 것을 감지하도록 구현
    intersection observer api를 image 각각에 적용시켜 뷰포트에 나타나는 것을 감지하도록 구현

--- src/components/PhotoImage.jsx
+++ src/components/PhotoImage.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
-const PhotoImage = ({ imageUrl }) => {
+const PhotoImage = ({ imageUrl, thumbnailUrl }) => {
+
+  const imageRef = useRef(null);
+
+  const onIntersect = ([entry], observer) => {
+    if(entry.isIntersecting) {
+      console.log("heelow");
+      observer.unobserve(entry.target);
+    }
+  }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersect, {threshold:0.1});
+    observer.observe(imageRef.current);
+
+    return() => {
+      observer && observer.disconnect();
+    }
+  }, [])
 
     return(
         <PhotoImageWrapper>
             <picture>
-                <img src={imageUrl} alt="Temp Image" />
+                <img src={imageUrl} alt="Temp Image" ref={imageRef} data-src={thumbnailUrl}/>
             </picture>
         </PhotoImageWrapper>
     )
